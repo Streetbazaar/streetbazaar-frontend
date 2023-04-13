3c79e5ab0d979d56ba43f75eb2f202dbb90709f2
danny235 <danielbarima235@gmail.com>
2023-04-13 16:49:33 +0100

    Changes to be committed (streetbazaar) - Added the categories images to the assets/images directory. - Added the main section to the home screen. - Added all color shades to the utils/color.js directory. - Created a styled input in the components/styledcomponents directory.

--- src/components/styledComponents.js
+++ src/components/styledComponents.js
@@ -5,13 +5,14 @@ const DivContainer = styled.div`
   padding: 20px;
 `;
 const HeaderContainer = styled.header`
-  padding: 20px;
+  padding: 15px;
   width: 100%;
   position: fixed;
   top: 0;
   left: 0;
   box-shadow: 1px 0 5px -2px var(--ash);
   background-color: ${Colors.white};
+  z-index: 1;
 `;
 
 const LgCaCtaBtn = styled.button`
@@ -26,9 +27,9 @@ const LgCaCtaBtn = styled.button`
   padding: 12px;
   align-items: center;
   color: ${Colors.neutral_color.color900};
-  `;
-  
-  const PrimaryBtn = styled.button`
+`;
+
+const PrimaryBtn = styled.button`
   outline: none;
   border: none;
   background: none;
@@ -39,11 +40,34 @@ const LgCaCtaBtn = styled.button`
   padding: 12px;
   align-items: center;
   color: ${Colors.white};
-  `;
-  
+  justify-content: center;
+`;
+
 const StyledLinks = styled.a`
   text-decoration: none;
   color: ${Colors.neutral_color.color900};
-`
+`;
 
-export { DivContainer, HeaderContainer, LgCaCtaBtn, PrimaryBtn, StyledLinks };
+const StyledInput = styled.input`
+  outline: none;
+  border: none;
+  background: none;
+  border: 1px solid ${Colors.neutral_color.color900};
+  border-radius: 2px;
+  padding: 10px;
+  width: 40%;
+`;
+
+const GridItem = styled.div`
+  background-color: ${(props) => props.bgColor};
+`;
+
+export {
+  DivContainer,
+  HeaderContainer,
+  LgCaCtaBtn,
+  PrimaryBtn,
+  StyledLinks,
+  StyledInput,
+  GridItem
+};
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,12 +1,70 @@
-import React from 'react'
-import Header from '../../components/header/Header'
+import React from "react";
+import Header from "../../components/header/Header";
+import jumbotron from "../../assets/images/jumbtron.png";
+import { Colors } from "../../utils/colors";
+import { GridItem, PrimaryBtn, StyledInput } from "../../components/styledComponents";
+import { SearchIconSvg } from "../../components/SvgComponents";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <main style={{ padding: "15px", maxWidth: "1440px", margin: "0 auto",  marginTop: 150,  }}>
+        <h1
+          style={{
+            color: Colors.neutral_color.color900,
+            width: "50%",
+            fontSize: 35,
+          }}
+        >
+          BUY, SELL, RENT AND EXCHANGE IN ONE CLICK
+        </h1>
+        <div
+          style={{
+            marginBottom: 20,
+            position: "relative",
+            display: "flex",
+            gap: 10,
+            marginTop: 40,
+          }}
+        >
+          <div style={{ position: "absolute", bottom: 8, left: 20 }}>
+            <SearchIconSvg />
+          </div>
+          <StyledInput
+            style={{ paddingLeft: 50, height: 50 }}
+            placeholder="Search for product"
+          />
+          <PrimaryBtn style={{ width: "10%" }}>Search</PrimaryBtn>
+        </div>
+        <img
+          src={jumbotron}
+          alt="Showcase Image"
+          style={{ width: "100%", aspectRatio: 2 / 1, marginBottom: 20 }}
+        />
+        <section>
+          <h2
+            style={{
+              color: Colors.neutral_color.color900,
+              fontSize: 25,
+              marginBottom: 20
+            }}
+          >
+            Categories
+          </h2>
+          <div className="categoriesGridContainer">
+            <GridItem bgColor={Colors.success_color.color100}>1</GridItem>
+            <GridItem bgColor={Colors.primary_color.color100}>2</GridItem>
+            <GridItem bgColor={Colors.lightPeach}>3</GridItem>
+            <GridItem bgColor={Colors.darkPeach}>4</GridItem>
+            <GridItem bgColor={Colors.warning_color.color100}>5</GridItem>
+            <GridItem bgColor={Colors.skyBlue}>6</GridItem>
+            <GridItem bgColor={Colors.warning_color.color90}>7</GridItem>
+          </div>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
